503dffe55723581837c267ff7ba320dffda98535
Julio Quinchiguango <jcquinchiguango@utn.edu.ec>
2021-01-28 15:38:56 -0500

    cambio ruta

--- Assets/js/admin/reportes.js
+++ Assets/js/admin/reportes.js
@@ -184,7 +184,7 @@ function generarPDF(cliente, factura) {
     var x = parseInt((window.screen.width / 2) - (ancho / 2));
     var y = parseInt((window.screen.height / 2) - (alto / 2));
 
-    $url = '../../factura/generaFactura.php?f=' + factura+'&cl=' + cliente;
+    $url =  base_url+'/factura/generaFactura.php?f=' + factura+'&cl=' + cliente;
     window.open($url, "Factura", "left=" + x + ",top=" + y + ",height=" + alto + ",width=" + ancho + ",scrollbar=si,location=no,resizable=si,menubar=no");
 }
 
